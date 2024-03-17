ce2e5f8e879c50525b0a42c9620853500651737e
rachelalkayal1 <163768610+rachelalkayal1@users.noreply.github.com>
2024-03-17 15:59:27 -0400

    Create tablecreation.sql

---
+++ Assignment3Q1/tablecreation.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students (
+	student_id			SERIAL	     	PRIMARY KEY,
+	first_name  		VARCHAR(225)	NOT NULL,
+	last_name   		VARCHAR(225)	NOT NULL,
+	email       		VARCHAR(255)	NOT NULL	UNIQUE,	
+	enrollment_date 	DATE 
+
+);
